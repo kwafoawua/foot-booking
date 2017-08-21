6ae21b555d1852f0d7038a8b6c17c19a86219a2c
Afua Kwafo-Awua <afua.kw@gmail.com>
2017-08-21 16:22:59 -0300

    Initial Commit

--- client/app/profile/profile.component.ts
+++ client/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 import { User } from '../_models/index';
 import { UserService } from '../_services/index';
