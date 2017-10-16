e4782c04399556f0e1823f5b8827cd871e30d65a
Afua Kwafo-Awua <afua.kw@gmail.com>
2017-10-16 11:27:22 -0300

    Se agrega carpeta Perfil de Club

---
+++ client/app/profile-club/index.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by USUARIO on 16/10/2017.
+ */
---
+++ client/app/profile-club/profile-club.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by USUARIO on 16/10/2017.
+ */
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    templateUrl: 'profile-club.component.html',
+    module: module.id
+})
+export class ProfileClubComponent implements OnInit{
+
+}
