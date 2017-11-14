2f33831400107d2db5456f66112d1bf25b8ce5aa
Pablo <pablotolaba91@gmail.com>
2017-11-14 12:03:35 -0300

    se mergea todo

--- client/app/ClubProfileClient/clubProfileClient.component.ts
+++ client/app/ClubProfileClient/clubProfileClient.component.ts
@@ -22,7 +22,7 @@ import * as moment from 'moment';
 export class ProfileClubClientComponent implements OnInit {
 
     bookingFilter : BookingFilter;
-    hoursArray: string [] = ["10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"];
+    hoursArray: string [] = ["10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00"];
     horasOcupadas: string [] = [];
     horasDisponibles: string [] = [];
     club : Club ;
