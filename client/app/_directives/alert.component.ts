7c2c619b96c2f946ff33a87cf1492534d9d0e9e8
Afua Kwafo-Awua <afua.kw@gmail.com>
2020-03-21 18:23:06 -0300

    style: indentation directives

--- client/app/_directives/alert.component.ts
+++ client/app/_directives/alert.component.ts
@@ -1,19 +1,21 @@
-﻿import { Component, OnInit } from '@angular/core';
-
+﻿import { Component } from '@angular/core';
 import { AlertService } from '../_services/index';
 
 @Component({
-    moduleId: module.id,
-    selector: 'alert',
-    templateUrl: 'alert.component.html'
+  moduleId: module.id,
+  selector: 'alert',
+  templateUrl: 'alert.component.html'
 })
 
 export class AlertComponent {
-    message: any;
+  message: any;
 
-    constructor(private alertService: AlertService) { }
+  constructor(private alertService: AlertService) {
+  }
 
-    ngOnInit() {
-        this.alertService.getMessage().subscribe(message => { this.message = message; });
-    }
+  ngOnInit() {
+    this.alertService.getMessage().subscribe(message => {
+      this.message = message;
+    });
+  }
 }
