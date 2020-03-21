65326f9dde3e59c2c38e98e6f740942f58603896
Afua Kwafo-Awua <afua.kw@gmail.com>
2020-03-21 19:00:10 -0300

    style: indentation confirmation booking player club profile estadisticas

--- client/app/booking-player/booking-player.component.ts
+++ client/app/booking-player/booking-player.component.ts
@@ -1,45 +1,34 @@
-import {Component, OnInit} from "@angular/core";
-import {Booking} from "../_models/booking";
-import {ActivatedRoute} from "@angular/router";
-import {ClubService} from "../_services/club.service";
-import {Player} from "../_models/player";
-import {BookingService} from "../_services/booking.service";
-import {PlayerService} from "../_services/player.service";
-/**
- * Created by pablo on 8/11/2017.
- */
+import { Component, OnInit } from '@angular/core';
+import { Booking } from '../_models/booking';
+import { ActivatedRoute } from '@angular/router';
+import { ClubService } from '../_services/club.service';
+import { Player } from '../_models/player';
+import { BookingService } from '../_services/booking.service';
+import { PlayerService } from '../_services/player.service';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'booking-player.html'
+  moduleId: module.id,
+  templateUrl: 'booking-player.html'
 })
 
+export class bookingPlayerComponent implements OnInit {
+  public bookings: any [] = [];
 
-export class bookingPlayerComponent implements OnInit{
+  constructor(private route: ActivatedRoute,
+              private playerService: PlayerService,
+              private clubService: ClubService,
+              private bookingService: BookingService) {
+  }
 
-    public bookings:any []=  [];
+  ngOnInit() {
+    const _id: string = JSON.parse(localStorage.getItem('currentUser')).playerOrClubId;
+    this.getBookings(_id);
+  }
 
-
-    constructor(private route: ActivatedRoute,
-                private playerService: PlayerService ,
-                private clubService: ClubService,
-                private bookingService: BookingService){
-    }
-
-    ngOnInit(){
-        const _id: string = JSON.parse(localStorage.getItem('currentUser')).playerOrClubId;
-
-        this.getBookings(_id);
-    }
-
-    private getBookings(_id: string){
-        this.bookingService.findAllByReferenceId(_id).subscribe((bookings)=>{
-            this.bookings = bookings;
-            console.log("esto",this.bookings);
-        });
-
-    }
-
-
-
+  private getBookings(_id: string) {
+    this.bookingService.findAllByReferenceId(_id).subscribe((bookings) => {
+      this.bookings = bookings;
+      console.log('esto', this.bookings);
+    });
+  }
 }
