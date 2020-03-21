952023de4f3ed986d1de4d4abbad07d323b01ad6
Afua Kwafo-Awua <afua.kw@gmail.com>
2020-03-21 18:34:08 -0300

    style: indentation services

--- client/app/_services/dateInRange.validatorService.ts
+++ client/app/_services/dateInRange.validatorService.ts
@@ -7,31 +7,31 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export class DateInRangeValidation {
-    
-    static validRange(min: Date, max: Date): ValidatorFn {
-        return (c: AbstractControl): { [key: string]: boolean } | null => {
-            if (c.value && (isNaN(c.value) || c.value < min || c.value > max)) {
-                return { 'range': false };
-            }
-            return null;
-        };
-    }
 
-    static validMaxRange(max: Date): ValidatorFn {
-        return (c: AbstractControl): { [key: string]: boolean } | null => {
-            if (c.value && (isNaN(c.value) || c.value > max)) {
-                return { 'range': false };
-            }
-            return null;
-        };
-    }
+  static validRange(min: Date, max: Date): ValidatorFn {
+    return (c: AbstractControl): { [ key: string ]: boolean } | null => {
+      if (c.value && (isNaN(c.value) || c.value < min || c.value > max)) {
+        return { 'range': false };
+      }
+      return null;
+    };
+  }
 
-    static validMinRange(min: Date): ValidatorFn {
-        return (c: AbstractControl): { [key: string]: boolean } | null => {
-            if (c.value && (isNaN(c.value) || c.value < min)) {
-                return { 'range': false };
-            }
-            return null;
-        };
-    }
+  static validMaxRange(max: Date): ValidatorFn {
+    return (c: AbstractControl): { [ key: string ]: boolean } | null => {
+      if (c.value && (isNaN(c.value) || c.value > max)) {
+        return { 'range': false };
+      }
+      return null;
+    };
+  }
+
+  static validMinRange(min: Date): ValidatorFn {
+    return (c: AbstractControl): { [ key: string ]: boolean } | null => {
+      if (c.value && (isNaN(c.value) || c.value < min)) {
+        return { 'range': false };
+      }
+      return null;
+    };
+  }
 }
--- client/app/_services/tournament.service.ts
+++ client/app/_services/tournament.service.ts
@@ -1,19 +1,17 @@
-import {Tournament} from "../_models/tournament";
-import {Http} from "@angular/http";
-import {Injectable} from "@angular/core";
-/**
- * Created by pablo on 25/11/2017.
- */
+import { Tournament } from '../_models/tournament';
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 
 export class TournamentService {
 
-    constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
-    create(tournament: any) {
-        console.log("El service ", tournament);
-        return this.http.post('/tournament/register', tournament);
-    }
+  create(tournament: any) {
+    console.log('El service ', tournament);
+    return this.http.post('/tournament/register', tournament);
+  }
 
 }
