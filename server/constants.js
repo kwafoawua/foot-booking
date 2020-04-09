69aa19338beebb049882b5a4d39c68a0e7b65ae6
Afua Kwafo - Awua <afua.kw@gmail.com>
2020-04-09 19:14:20 -0300

    [Server]: Refactor userController module (#15)
    * feat: create constants file
    
    * chore: update dependencies
    
    * refactor: replace Q promise with await/async

---
+++ server/constants.js
@@ -0,0 +1,16 @@
+const Errors = {
+  NO_USER: 'El usuario no existe',
+  INVALID_USER: 'Usuario o contraseña incorrecto',
+  EMAIL_USED: 'El email ya está en uso',
+  NO_UPDATE_PASSWORD: 'No se pudo actualizar la contraseña',
+};
+
+const SuccessMessage = {
+  SUCCESS_EMAIL: 'El email se actualizó con éxito',
+  SUCCESS_PASSWORD: 'La contraseña se actualizó con éxito',
+};
+
+module.exports = {
+  Errors,
+  SuccessMessage,
+};
