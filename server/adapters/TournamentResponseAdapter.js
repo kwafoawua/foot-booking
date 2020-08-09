0050c08d628e27236591762b6601c55556697164
Andres Giacosa <andres.giacosa@claro.com.ar>
2020-08-09 20:01:55 -0300

    Cambia nombre de campo que muestra el numero de inscripciones

--- server/adapters/TournamentResponseAdapter.js
+++ server/adapters/TournamentResponseAdapter.js
@@ -4,7 +4,7 @@ exports.apply = async (tournaments) => {
     var response = [];
     for (let t of tournaments) {
         var doc = t._doc;
-        var tmp = [{tournament: doc, inscNumb: await tournamentUtils.getNumberOfInscriptions(t._doc._id)}];
+        var tmp = [{tournament: doc, inscriptionNumber: await tournamentUtils.getNumberOfInscriptions(t._doc._id)}];
         response = response.concat(tmp);
     }
     return response;
