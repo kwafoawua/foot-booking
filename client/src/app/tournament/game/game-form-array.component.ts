517d0cd138739afa52529a9e5c24b5a7b9a77f21
Afua Kwafo-Awua <afua.kw@gmail.com>
2020-08-12 22:04:10 -0300

    fix de errores para levantar el proyecto

--- client/src/app/tournament/game/game-form-array.component.ts
+++ client/src/app/tournament/game/game-form-array.component.ts
@@ -11,9 +11,9 @@ export class GameFormArrayComponent {
   @Input()
   public GameFormArray: FormArray;
 
-  addGame() {
-    this.GameFormArray.push(GameComponent.buildGame())
-  }
+ // addGame() {
+ //   this.GameFormArray.push(GameComponent.buildGame())
+ // }
 
   // static initGames() {
   //   return new FormArray([
