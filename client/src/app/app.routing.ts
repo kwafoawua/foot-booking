5712ae3b8603cb5bffec8a058a93703bc154a9da
Afua Kwafo-Awua <afua.kw@gmail.com>
2020-08-18 21:04:01 -0300

    se agrega layout vacio de admin

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,5 +1,7 @@
 ﻿import { Routes, RouterModule } from '@angular/router';
 
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { AdminLayoutComponent } from './_layout/admin-layout/admin-layout.component';
 import { HomeComponent } from './home/index';
 import { LoginComponent } from './login/index';
 import { RegisterClubComponent } from './register-club/index';
@@ -16,21 +18,28 @@ import {MainManagementComponent} from './tournament-management/mainManagement.co
 import {ClubInfoComponent} from './ClubProfileClient/clubInfo.component';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  {
+    path: '',
+    component: SiteLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
 
-  { path: 'login', component: LoginComponent, canActivate: [ LoginGuard ] },
-  { path: 'results/club/:id', component: ClubInfoComponent },
-  { path: 'confirmation', component: ConfirmationComponent },
-  { path: 'player/register', component: RegisterPlayerComponent },
-  { path: 'profile-player/:id', component: ProfilePlayerComponent, canActivate: [ AuthGuard ] },
-  { path: 'results', component: ResultComponent },
-  { path: 'club/register', component: RegisterClubComponent },
-  { path: 'club/admin', component: AdminClubComponent, canActivate: [ AuthGuard ] },
-  { path: 'club/estadisticas', component: EstadisticasClubComponent },
-  // { path: 'club/gestion-canchas', component: FieldsManagementComponent },
-  { path: 'player/mis-reservas', component: bookingPlayerComponent },
-  { path: 'campeonatos/administrar', component: MainManagementComponent},
-  { path: '**', redirectTo: '' }
+      { path: 'login', component: LoginComponent, canActivate: [ LoginGuard ] },
+      { path: 'results/club/:id', component: ClubInfoComponent },
+      { path: 'confirmation', component: ConfirmationComponent },
+      { path: 'player/register', component: RegisterPlayerComponent },
+      { path: 'profile-player/:id', component: ProfilePlayerComponent, canActivate: [ AuthGuard ] },
+      { path: 'results', component: ResultComponent },
+      { path: 'club/register', component: RegisterClubComponent },
+      { path: 'club/admin', component: AdminClubComponent, canActivate: [ AuthGuard ] },
+      { path: 'club/estadisticas', component: EstadisticasClubComponent },
+      // { path: 'club/gestion-canchas', component: FieldsManagementComponent },
+      { path: 'player/mis-reservas', component: bookingPlayerComponent },
+      { path: 'campeonatos/administrar', component: MainManagementComponent},
+      { path: '**', redirectTo: '' }
+    ]
+  },
+
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
