93df994d30ed310cce9640e4830bfdaef6b6ea89
pablotolaba91@gmail.com <pablotolaba91@gmail.com>
2020-09-25 23:45:58 -0300

    modifico inscripcion campeonato

---
+++ client/src/app/ClubProfileClient/CampeonatoInscripcion.component.ts
@@ -0,0 +1,59 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Tournament} from '../_models/tournament';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {TournamentService} from '../_services/tournament.service';
+import {AlertService} from '../_services';
+
+@Component({
+  templateUrl: './CampeonatoInscripcion.component.html',
+  selector: 'camp-insc'
+})
+
+export class CampeonatoInscripcionComponent implements OnInit{
+  @Input() torneo: Tournament;
+  inscripcionForm: FormGroup;
+  isLinear: false;
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  constructor(private route: ActivatedRoute,
+              private tournamentService: TournamentService,
+              private fb: FormBuilder,
+              private alertService: AlertService) {
+  }
+
+
+  createForm() {
+    this.inscripcionForm = this.fb.group({
+      idTournament: [''],
+      nombreEquipo: [null, Validators.required, Validators.maxLength(50), Validators.minLength(5)],
+      nombreResponsable: [null, Validators.required, Validators.maxLength(50), Validators.minLength(5)],
+      //apellidoResponsable: [null, Validators.required],
+      //email: [null, Validators.required],
+      telefono: [null, Validators.required, Validators.maxLength(12), Validators.minLength(1)],
+    });
+  }
+
+  Inscribir() {
+    this.inscripcionForm.controls['idTournament'].setValue(this.route.snapshot.params.id) ;
+    if (this.inscripcionForm.valid) {
+      console.log('ël form validado', this.inscripcionForm);
+      {
+        this.tournamentService.createInscription(this.inscripcionForm.value).subscribe(data => {
+            this.alertService.success('La inscripción se registro con éxito', true),
+              console.log('el form', this.inscripcionForm);
+          },
+          error => {
+            this.alertService.error(error, false);
+          }
+        );
+      }
+      this.createForm();
+    }
+  }
+
+
+}
