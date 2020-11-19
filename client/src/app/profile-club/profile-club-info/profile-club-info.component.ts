53124a8512b20288fb4a892aa368a0ae80827bab
pablotolaba91@gmail.com <pablotolaba91@gmail.com>
2020-11-19 19:55:23 -0300

    Se puede ver el fixture cuando el campeonato esta finalizado

--- client/src/app/profile-club/profile-club-info/profile-club-info.component.ts
+++ client/src/app/profile-club/profile-club-info/profile-club-info.component.ts
@@ -24,6 +24,11 @@ export class ProfileClubInfoComponent implements OnInit {
   galleryToUpload: File[] = [];
   loading = false;
   uploadsBaseURL = environment.uploadsBaseURL;
+  lat: number;
+  lng: number;
+  icon: '../../assets/icon/iconochico.png';
+  zoom: number;
+  draggable = true; // Necesario para el que el marcador del mapa se mueva
 
 
   constructor(private clubService: ClubService,
@@ -213,6 +218,63 @@ export class ProfileClubInfoComponent implements OnInit {
     } else {
       ValidateAllFields.validateAllFields(this.clubForm);
     }
+  }
+
+  // se actualiza la ubicacion de lat y log al terminar de arrastrar el marcador
+  nuevaPosicionMarcador(e) {
+    this.lat = e.coords.lat;
+    this.lng = e.coords.lng;
+    this.setAutocompleteInput();
+    console.log('nueva lat ' + this.lat, 'nueva lng' + this.lng);
+  }
+
+  setAutocompleteInput() {
+    const geocoder = new google.maps.Geocoder();
+    const latlng = new google.maps.LatLng(this.lat, this.lng);
+    const request = {
+      location: latlng
+    };
+    geocoder.geocode(request, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+
+        if (results[0] != null) {
+          let city = '';
+          let country = '';
+          let provincia = '';
+          this.searchElementRef.nativeElement.value = results[ 0 ].formatted_address;
+          this.clubForm.get('address.lat').setValue(results[ 0 ].geometry.location.lat());
+          this.clubForm.get('address.lng').setValue(results[ 0 ].geometry.location.lng());
+          this.clubForm.get('address.address').setValue(results[ 0 ].formatted_address);
+          results[0].address_components.forEach(addr => {
+            switch (addr.types[0]) {
+              case 'administrative_area_level_2':
+                city = addr.short_name;
+                break;
+              case 'administrative_area_level_1':
+                provincia = addr.short_name;
+                break;
+              case 'country':
+                country = addr.long_name;
+            }
+          });
+          this.clubForm.get('address.shortAddress').setValue(`${city}, ${provincia}, ${country}`);
+
+        } else {
+          alert('No address available');
+        }
+      }
+    });
+  }
+
+  // El primer click sobre el mapa
+  clickMapa(e) {
+    this.lng = e.coords.lng;
+    this.lat = e.coords.lat;
+    this.setAutocompleteInput();
+
+    console.log('lat' + this.lat, 'long' + this.lng);
+    //     this.registerClubForm.address.lat = e.coords.lat;
+    //     this.registerClubForm.address.lng=e.coords.lng;
   }
 
 }
