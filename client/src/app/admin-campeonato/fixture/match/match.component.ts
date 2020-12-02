33ca433f41c58a3bf1b1ae949a0efa0cf1b848dd
Afua Kwafo-Awua <afua.kw@gmail.com>
2020-12-02 17:03:49 -0300

    implementacion inicial de form de canchas en partido

--- client/src/app/admin-campeonato/fixture/match/match.component.ts
+++ client/src/app/admin-campeonato/fixture/match/match.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Inject, Input, OnInit, Output, ViewEncapsulati
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import { DatePipe } from '@angular/common';
 import * as moment from 'moment';
+import { BookingFilter } from '../../../_models/bookingfilter';
+import { BookingService } from '../../../_services';
 
 export interface IMatch {
   match: {
@@ -9,6 +11,8 @@ export interface IMatch {
     id: string;
     round: string;
     hourDate: string;
+    dateToPlay: Date;
+    field: any;
     teams: [{
       name: string;
       score: string;
@@ -19,6 +23,9 @@ export interface IMatch {
       }]
   };
   teams: any[];
+  fields: any[];
+  maxDate: Date;
+  minDate: Date;
 }
 
 @Component({
@@ -33,6 +40,9 @@ export class MatchComponent implements OnInit {
   @Input() teams: any;
   @Output() updateMatch = new EventEmitter<IMatch>();
   @Input() isCampeonato: boolean;
+  @Input() fields: any;
+  @Input() maxDate: Date;
+  @Input() minDate: Date;
   tooltipText: string;
   pipe = new DatePipe('es-AR'); // Use your own locale
 
@@ -54,8 +64,14 @@ export class MatchComponent implements OnInit {
 
   openDialog(match): void {
     const dialogRef = this.dialog.open(MatchUpdateDialogComponent, {
-      width: '40%',
-      data: { match: this.match, teams: this.teams }
+      width: '60%',
+      data: {
+        match: this.match,
+        teams: this.teams,
+        fields: this.fields,
+        maxDate: this.maxDate,
+        minDate: this.minDate
+      }
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -74,9 +90,18 @@ export class MatchComponent implements OnInit {
 export class MatchUpdateDialogComponent implements OnInit{
   match: any;
   inscriptions: any;
+  fieldDropdown: any;
+  horasDisponibles: any;
+  horasOcupadas: any;
+  hoursArray: string [] = [ '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00' ];
+  filter: BookingFilter;
+  fields: any;
   disabledScore = false;
+  maxDate: Date;
+  minDate: Date;
   constructor(
     public dialogRef: MatDialogRef<MatchUpdateDialogComponent>,
+    private bookingService: BookingService,
     @Inject(MAT_DIALOG_DATA) public data: IMatch) {}
 
   onNoClick(): void {
@@ -87,6 +112,30 @@ export class MatchUpdateDialogComponent implements OnInit{
     this.match = this.data.match;
     this.inscriptions = this.data.teams;
     this.disabledScore = moment(this.match.dateToPlay).isSameOrAfter(new Date());
+    this.fields = this.data.fields;
+    this.maxDate = this.data.maxDate;
+    this.minDate = this.data.minDate;
+  }
+
+  setFieldValues(field: any) {
+    console.log(field);
+  }
+
+  loadHoursValues(date: any) {
+    this.horasDisponibles = [];
+    this.horasOcupadas = [];
+    this.filter = new BookingFilter(this.fieldDropdown, date);
+    this.bookingService.findAllBookingsByFieldAndDay(this.filter).subscribe(hoursBooking => {
+        if (hoursBooking.length) {
+          hoursBooking.forEach((booking, index) => {
+            this.horasOcupadas.push(booking.playingTime);
+            this.horasDisponibles = this.hoursArray.filter(item => this.horasOcupadas.indexOf(item) < 0);
+          });
+        } else {
+          this.horasDisponibles = this.hoursArray;
+        }
+      });
+
   }
 
 }
