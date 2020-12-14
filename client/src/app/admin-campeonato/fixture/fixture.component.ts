bd2a14faf81f73fa44101b0ff8ba78812ddf4102
pablotolaba91@gmail.com <pablotolaba91@gmail.com>
2020-12-13 21:57:58 -0300

    se agrega - cuando no hay equipos

--- client/src/app/admin-campeonato/fixture/fixture.component.ts
+++ client/src/app/admin-campeonato/fixture/fixture.component.ts
@@ -211,8 +211,8 @@ myTournamentData: any;
   }
 
   mapPhaseToMatch(match, phaseId, phaseType, tournamentId) {
-    const localTeamName = !fixtureRegexp(match.localTeam.teamName) ? match.localTeam.teamName : null;
-    const visitorTeamName = !fixtureRegexp(match.visitorTeam.teamName) ? match.visitorTeam.teamName : null;
+    const localTeamName = !fixtureRegexp(match.localTeam.teamName) ? match.localTeam.teamName : '-';
+    const visitorTeamName = !fixtureRegexp(match.visitorTeam.teamName) ? match.visitorTeam.teamName :  '-';
 
     return {
       field: null,
